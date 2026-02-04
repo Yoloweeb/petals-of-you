1e0f9263bee09ca316ecf7e2ffa032f405a91854
Yoloweeb <55025908+Yoloweeb@users.noreply.github.com>
2026-02-04 09:10:09 +0100

    Add start menu scene with title image

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,1 +1,17 @@
-extends Node
+extends Control
+
+const WITHER_SCENE := "res://scenes/levels/wither.tscn"
+
+@onready var start_button: Button = $CenterContainer/PanelContainer/VBoxContainer/TitleImage/Start
+@onready var quit_button: Button = $CenterContainer/PanelContainer/VBoxContainer/TitleImage/Quit
+
+func _ready() -> void:
+	start_button.grab_focus()
+	
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file(WITHER_SCENE)
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
